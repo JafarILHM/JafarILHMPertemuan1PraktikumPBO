b07a8263ab92390605657ffda9e21cbe5cf71b0f
unknown <jafaruddinilham15062005@gmail.com>
2024-09-05 14:15:38 +0700

    Create JafarILHMPertemuan1PraktikumPBO

---
+++ Mahasiswa.java
@@ -0,0 +1,15 @@
+package JafarILHMPertemuan1PraktikumPBO;
+
+class Mahasiswa {
+    void membaca() {
+        System.out.println("Mahasiswa sedang membaca buku.");
+    }
+
+    void nyontek() {
+        System.out.println("Mahasiswa sedang nyontek saat ujian.");
+    }
+
+    void modifikasi() {
+        System.out.println("Mahasiswa sedang memodifikasi proyek.");
+    }
+}
---
+++ MahasiswaBeraksi.java
@@ -0,0 +1,11 @@
+package JafarILHMPertemuan1PraktikumPBO;
+
+public class MahasiswaBeraksi {
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+        
+        mhs.membaca();
+        mhs.nyontek();
+        mhs.modifikasi();
+    }
+}
